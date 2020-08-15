7152d5c9bd24c6db664cd5799e5154ec4e46bba1
Jorge Danilo <sobrejavatec@gmail.com>
2020-08-15 09:39:19 -0300

    implementation modal form with show rounded date picker to selected time and date

--- lib/widgets/week_view_widget.dart
+++ lib/widgets/week_view_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_rounded_date_picker/rounded_picker.dart';
 import 'package:flutter_week_view/flutter_week_view.dart';
 import 'package:schedule/widgets/text.dart';
 
@@ -25,7 +26,8 @@ class _WeekViewWidgetState extends State<WeekViewWidget> {
           IconButton(
             onPressed: () {
               setState(() {
-                var start = DateTime(now.year, now.month, now.day, Random().nextInt(24), Random().nextInt(60));
+                var start = DateTime(now.year, now.month, now.day,
+                    Random().nextInt(24), Random().nextInt(60));
                 events.add(
                   FlutterWeekViewEvent(
                       onTap: () => showSchedule(context),
@@ -66,57 +68,107 @@ class _WeekViewWidgetState extends State<WeekViewWidget> {
   }
 
   void showForm() {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     showDialog(
-        context: context,
-        builder: (_) => new AlertDialog(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10.0),
-                ),
-              ),
-              content: Builder(
-                builder: (context) {
-                  var height = MediaQuery.of(context).size.height;
-                  var width = MediaQuery.of(context).size.width;
-                  return Container(
-                    height: height * 0.5,
-                    width: width * 0.2,
-                    child: Column(
-                      children: <Widget>[
-                        Center(
-                          child: text("Nova Agenda", fontSize: 20, bold: true, color: Colors.purple[600]),
+      context: context,
+      builder: (_) => new AlertDialog(
+        title:  Center(
+          child: text("Nova Agenda",
+              fontSize: 20, bold: true, color: Colors.purple[600]),
+        ),
+        contentPadding: EdgeInsets.all(15.0),
+        insetPadding: EdgeInsets.symmetric(horizontal: 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(11.0),
+          ),
+        ),
+        content: Builder(
+          builder: (context) {
+            return Container(
+              padding: EdgeInsets.all(0),
+              height: height * 0.5,
+              width: width + 400,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    onTap: () async {
+                      await showRoundedDatePicker(
+                        theme: ThemeData(
+                          primaryColor: Colors.purple[600],
+                          accentColor: Colors.purple[600],
                         ),
-                        SizedBox(height: 20,),
-                        TextFormField(
-                          decoration: InputDecoration(
-                             icon: Icon(Icons.title, color: Colors.purple[600],),
-                              labelText: 'Nome',
-                              labelStyle: TextStyle(color: Colors.purple[600]),
-                          ),
-                        ),
-                        TextFormField(
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null,
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.description, color: Colors.purple[600],),
-                            labelText: 'Descrição',
-                            labelStyle: TextStyle(color: Colors.purple[600]),
-                          ),
-                        ),
-                        SizedBox(height: 100,),
-                        Container(
-                          width: 300,
-                          child: RaisedButton(
-                            onPressed: () {},
-                            color: Colors.purple[600],
-                            child: text("Salvar", color: Colors.white),
-                          ),
-                        )
-                      ],
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(DateTime.now().year - 1),
+                          lastDate: DateTime(DateTime.now().year + 1),
+                          borderRadius: 16,
+                      );
+                    },
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.date_range,
+                        color: Colors.purple[600],
+                      ),
+                      labelText: 'Data',
+                      labelStyle: TextStyle(color: Colors.purple[600]),
                     ),
-                  );
-                },
+                  ),
+                  TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.hourglass_empty,
+                        color: Colors.purple[600],
+                      ),
+                      labelText: 'Hora',
+                      labelStyle: TextStyle(color: Colors.purple[600]),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.title,
+                        color: Colors.purple[600],
+                      ),
+                      labelText: 'Nome',
+                      labelStyle: TextStyle(color: Colors.purple[600]),
+                    ),
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.description,
+                        color: Colors.purple[600],
+                      ),
+                      labelText: 'Descrição',
+                      labelStyle: TextStyle(color: Colors.purple[600]),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 300,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: Colors.purple[600],
+                      child: text("Salvar", color: Colors.white),
+                    ),
+                  )
+                ],
               ),
-            ));
+            );
+          },
+        ),
+      ),
+    );
   }
 }
